901f8d32f3e961cfabb571acbb49412f0955c89e
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-22 18:52:57 +0000

    user controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
+// signup working
 exports.signup = (req, res, next) => {
 	bcrypt.hash(req.body.password, 10).then((hash) => {
 		const user = new User({
@@ -25,6 +26,7 @@ exports.signup = (req, res, next) => {
 	});
 };
 
+// login working
 exports.login = (req, res, next) => {
 	User.findOne({email: req.body.email})
 		.then((user) => {
@@ -59,3 +61,105 @@ exports.login = (req, res, next) => {
 			});
 		});
 };
+
+
+// get all users working
+exports.getUsers = async (req, res) => {
+		try {
+		  const users = await User.find();
+		  res.json(users);
+		} catch (error) {
+		  console.error(error);
+		  res.status(500).json({ error: 'Internal server error' });
+		}
+}
+
+// delete user working
+exports.deleteUser = async (req, res, next) => {
+	try {
+		const user = await User.findByIdAndDelete(req.params.id);
+		if (!user) {
+		  return res.status(404).json({ error: 'User not found' });
+		}
+		res.json({ message: 'User deleted' });
+	  } catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal server error' });
+	  }
+}
+
+// Find user by ID - working
+exports.getUserbyID = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+		  return res.status(404).json({ error: 'User not found' });
+		}
+		res.json(user);
+	  } catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal server error' });
+	  }
+}
+
+// Create user
+exports.createUser = async (req, res) => {
+	try {
+		const user = new User(req.body);
+		await User.save();
+		res.status(201).json({ message: 'Student created', user });
+	  } catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal server error' });
+	  }
+}
+
+// update user BUG
+exports.updateUser = async (req, res) => {
+	try {
+		const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+		if (!user) {
+		  return res.status(404).json({ error: 'User not found' });
+		}
+		res.json(user);
+	  } catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal server error' });
+	  }
+}
+ 
+// app.get('/api/logs', async (req, res) => {
+// 	try {
+// 	  const Log = mongoose.model('Log', {
+// 		ID: String,
+// 		loginTime: Date,
+// 		logoutTime: Date,
+// 	  });
+// 	  const logs = await Log.find();
+// 	  res.json(logs);
+// 	} catch (error) {
+// 	  console.error(error);
+// 	  res.status(500).json({ error: 'Internal server error' });
+// 	}
+//   });
+   
+ 
+//   // get user by username
+//   app.get('/api/students/username/:username', async (req, res) => {
+// 	try {
+// 	  const student = await Student.findOne({ username: req.params.username });
+// 	  if (!student) {
+// 		return res.status(404).json({ error: 'Student not found' });
+// 	  }
+// 	  res.json(student);
+// 	} catch (error) {
+// 	  console.error(error);
+// 	  res.status(500).json({ error: 'Internal server error' });
+// 	}
+//   });
+  
+  
+
+  
+  
+
