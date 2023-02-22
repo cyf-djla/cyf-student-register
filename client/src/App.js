8e08fa36bbfdf9d995776d0b168073eb4a90039f
Lunamzelo <lunamzelo@gmail.com>
2023-02-22 15:57:00 +0000

    edited css

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,11 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Layout from "./Layout";
 
-
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/Layout" element={<Layout />} />
-
         <Route path="/" element={<Home />} />
       </Routes>
       {/* <Layout/> */}
