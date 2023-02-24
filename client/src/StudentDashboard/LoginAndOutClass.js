5fa6743968e2de6bc634f60acb57590cf9e8ee5c
Lunamzelo <lunamzelo@gmail.com>
2023-02-24 11:45:54 +0000

    updated dashboard

---
+++ client/src/StudentDashboard/LoginAndOutClass.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./LoginAndOutClass.css";
+const LoginAndOutClass = () => {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate.push("/Dashboard2");
+  }
+  return (
+    <div className="buttons">
+      <button className="login__class" onClick={handleClick}>
+        Log in class
+      </button>
+      <button className="logout__class" onClick={handleClick}>
+        Log out class
+      </button>
+    </div>
+  );
+};
---
+++ client/src/StudentDashboard/LoginTime.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginTime = () => {
+  return (
+    <div>
+      <label> Login time / date</label>
+      <input placeholder="--:-- on --/--/-----" />
+    </div>
+  );
+};
+
+export default LoginTime;
