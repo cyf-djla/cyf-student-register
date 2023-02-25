6d4f5eb8e3dbe875f104bcfc87589f4c4a3de212
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-25 22:33:39 +0000

    volunteer login

--- server/controllers/class.js
+++ server/controllers/class.js
@@ -1,24 +1,6 @@
 const Class = require("../models/class");
 
 // sign into class
-// exports.traineeClassSignIn = async (req, res, next) => {
-// 	try {
-// 		let user = {name: req.body.name, logintime: new Date(Date.now())};
-
-// 		const todaysclass = await Class.findOne({_id: req.params.id});
-
-// 		if (!todaysclass.trainees.includes(user)) {
-// 			todaysclass.trainees.push(user);
-// 		}
-
-// 		const updatedClass = await todaysclass.save();
-
-// 		res.status(200).json({todaysclass: updatedClass});
-// 	} catch (error) {
-// 		res.status(400).json({error: error.message});
-// 	}
-// };
-
 exports.traineeClassSignIn = async (req, res, next) => {
 	try {
 		let user = {name: req.body.name, logintime: new Date(Date.now())};
@@ -26,11 +8,9 @@ exports.traineeClassSignIn = async (req, res, next) => {
 		const todaysclass = await Class.findOne({_id: req.params.id});
 
 		const traineeIndex = todaysclass.trainees.findIndex((trainee) => trainee.name === user.name);
-		if (traineeIndex === -1) {
-			// Trainee not found in trainees array
+		if (traineeIndex === -1) {		
 			todaysclass.trainees.push(user);
 		} else {
-			// Trainee found in trainees array
 			todaysclass.trainees[traineeIndex].logintime = user.logintime;
 		}
 
@@ -50,11 +30,9 @@ exports.traineeClassSignOut = async (req, res, next) => {
 
 		const traineeIndex = todaysclass.trainees.findIndex((trainee) => trainee.name === user.name);
 		if (traineeIndex === -1) {
-			// Trainee not found in trainees array
 			res.status(400).json({error: "Trainee not found"});
 			return;
 		} else {
-			// Trainee found in trainees array
 			todaysclass.trainees[traineeIndex].logouttime = user.logouttime;
 		}
 
@@ -72,15 +50,18 @@ exports.volunteerClassSignIn = async (req, res, next) => {
 
 		const todaysclass = await Class.findOne({_id: req.params.id});
 
-		if (!todaysclass.volunteers.includes(user)) {
+		const volunteerIndex = todaysclass.volunteers.findIndex((volunteer) => volunteer.name === user.name);
+		if (volunteerIndex === -1) {		
 			todaysclass.volunteers.push(user);
+		} else {
+			todaysclass.volunteers[volunteerIndex].logintime = user.logintime;
 		}
 
 		const updatedClass = await todaysclass.save();
 
 		res.status(200).json({todaysclass: updatedClass});
 	} catch (error) {
-		res.status(400).json({error: error.message});
+		res.status(400).json({error: error});
 	}
 };
 
