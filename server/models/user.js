2b22834ac465a266cee083c60896aeb4db77e08a
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-28 07:51:40 +0000

    changed model for flags

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,9 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
 	email: {type: String, required: true, unique: true},
+	userID: {type: Number, required: true, unique:true},
 	password: {type: String, required: true},
-	username: {type: String, required: true},
+	username: {type: String, required: true,unique: true},
 	cohort: {type: String, required: true},
 	isVolunteer: {type: Boolean,required:true, default:false}
 });
