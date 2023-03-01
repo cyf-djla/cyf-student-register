ab47fccc63b4a6235d57aee8a212a1099b7c6ac2
12shussein <drahimhussein@gmail.com>
2023-03-01 22:20:23 +0000

    uncommented VolunteerDashboard

--- client/src/VolunteerDashboard.js
+++ client/src/VolunteerDashboard.js
@@ -1,24 +1,24 @@
-// import React from "react";
-// import Header from "./VolunteerDashboard_Comps/Header.js";
-// //import "./VolunteerDashboard_Comps/Layout.css";
+import React from "react";
+import Header from "./VolunteerDashboard_Comps/Header.js";
+import "./VolunteerDashboard_Comps/Layout.css";
 
-// import VolunteerDetail from "./VolunteerDashboard_Comps/VolunteerDetail";
-// import DisplayTime from "./VolunteerDashboard_Comps/DisplayTime";
-// import Modules from "./VolunteerDashboard_Comps/DropdownMenu";
-// import LoginAndOutClass from "./VolunteerDashboard_Comps/LoginAndOutClass";
-// const VolunteerDashboard = ({ children }) => {
-//   return (
-//     <div>
-//       <Header />
-//       {children}
-//       <div className="body">
-//         <VolunteerDetail />
-//         <DisplayTime />
-//         <Modules />
-//         <LoginAndOutClass />
-//       </div>
-//     </div>
-//   );
-// };
+import VolunteerDetail from "./VolunteerDashboard_Comps/VolunteerDetail";
+import DisplayTime from "./VolunteerDashboard_Comps/DisplayTime";
+import Modules from "./VolunteerDashboard_Comps/DropdownMenu";
+import LoginAndOutClass from "./VolunteerDashboard_Comps/LoginAndOutClass";
+const VolunteerDashboard = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <div className="body">
+        <VolunteerDetail />
+        <DisplayTime />
+        <Modules />
+        <LoginAndOutClass />
+      </div>
+    </div>
+  );
+};
 
-// export default VolunteerDashboard;
+export default VolunteerDashboard;
