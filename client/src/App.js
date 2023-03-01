c109d41dbc6144798eeb557645a4fc34d425c572
12shussein <drahimhussein@gmail.com>
2023-03-01 18:46:03 +0000

    completed new volunteer sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -5,40 +5,33 @@ import Layout from "./StudentDashboard/Layout"
 import TraineeLogin from "./TraineeLogin";
 import About from "./StudentDashboard/About";
 import GoodBye from "./StudentDashboard/GoodBye";
+import TraineeRegister from './TraineeRegister';
 
-//import TraineeRegister from './TraineeRegister';
-
-//import MainMenu from './MainMenu';
+import MainMenu from './MainMenu';
 import VolunteerDashboard from "./VolunteerDashboard";
-//import VolunteerMenu from './VolunteerMenu';
+import VolunteerMenu from './VolunteerMenu';
+import VolunteerRegister from './VolunteerRegister';
 import "./index.css";
 
 
 
 function App() {
   return (
-    // <main className="App">
-    //   <img src="/img/images.jpg" alt="cyf logo" className="img" />
-    //   {showLogin ? <Login /> : <Register />}
-    //   <button onClick={handleShowLogin}>
-    //     {showLogin ? "Show Register" : "Show Login"}
-    //   </button>
-    // </main>
+
     <Routes>
       <Route path="/bye" element={<GoodBye />} />
 
       <Route path="/about" element={<About />} />
 
-      <Route path="/" element={<TraineeLogin />} />
-      <Route path="/Layout" element={<Layout />} />
-    {/* /</Routes>
-      <Routes> */}
-        {/* <Route path="/TraineeRegister" element={<TraineeRegister />} /> */}
-        {/* <Route path="/TraineeLogin" element={<TraineeLogin />} /> */}
-        {/* <Route path="/VolunteerMenu" element={<VolunteerMenu />} /> */}
-{/* 
-        <Route path="/Layout" element={<Layout />} /> */}
-        {/* <Route path="/" element={<MainMenu/>} /> */}
+      {/* <Route path="/" element={<TraineeLogin />} />
+      <Route path="/Layout" element={<Layout />} /> */}
+
+        <Route path="/TraineeRegister" element={<TraineeRegister />} /> 
+        <Route path="/TraineeLogin" element={<TraineeLogin />} /> 
+        <Route path="/VolunteerMenu" element={<VolunteerMenu />} /> 
+        <Route path="/VolunteerRegister" element={<VolunteerRegister />} /> 
+        <Route path="/Layout" element={<Layout />} /> 
+        <Route path="/" element={<MainMenu/>} /> 
         <Route path="/VolunteerDashboard" element={<VolunteerDashboard/>} />
       </Routes>
   );
