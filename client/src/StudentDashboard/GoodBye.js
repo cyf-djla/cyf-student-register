5100377695b99122522bfcec44ad31ed0d0763d8
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-02 09:58:11 +0000

    log out from app

--- client/src/StudentDashboard/GoodBye.js
+++ client/src/StudentDashboard/GoodBye.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const GoodBye = () => {
+  fetch('http://127.0.0.1:4200/api/auth/logout', {
+      method: "get",
+      headers: {
+        "Content-type": "application/json"
+      },
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
   return (
     <div className='good__bye' >
       <p>See You Next Class!</p>
