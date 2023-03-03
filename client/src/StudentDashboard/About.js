eca0db9d8e52e7a56e19f1270d5a3ca24ddca456
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-03 22:27:26 +0000

    css

--- client/src/StudentDashboard/About.js
+++ client/src/StudentDashboard/About.js
@@ -5,13 +5,14 @@ const About = () => {
     <div>
       <Header />
       <div className="about__us">
-        <h3>About Us</h3>
+        <h1>About Us</h1>
         <p>
           CODE YOUR FUTURE <br /> need to keep record of Trainee attendance,
           punctuality and behavior during classes. This allows volunteers to
           provide the best possible service for trainees, with expense and
           teaching support, and mentorship.
-          <br /> Our mission:
+          <br/>
+          <br /> OUR MISSION:
           <br /> Our vision is an application that'll allow students to
           digitally log into classes via mobile or laptop device easily and
           efficiently. Their participation then stored to a database, accessed
