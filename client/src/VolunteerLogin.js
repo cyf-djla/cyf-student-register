79b08b64bb6f669801c8219dbf26c6793a523be5
12shussein <drahimhussein@gmail.com>
2023-03-03 14:38:47 +0000

    completed a small change2

--- client/src/VolunteerLogin.js
+++ client/src/VolunteerLogin.js
@@ -105,107 +105,11 @@ const VolunteerLogin = () => {
         <p>
           Need an Account?
           <br />
-          <i>Click on the button below to sign up</i>
+          <i>Click on the Menu at the top right to return to the home page</i>
         </p>
       </section>
     </>
   );
 };
-// 	const userRef = useRef();
-// 	const errRef = useRef();
-
-// 	const [email, setEmail] = useState("");
-// 	const [password, setPassword] = useState("");
-// 	const [errMsg, setErrMsg] = useState("");
-// 	const [errorMessage, setErrorMessage] = useState("");
-// 	const [isMounted, setIsMounted] = useState(false);
-
-// 	useEffect(() => {
-// 		setIsMounted(true);
-// 		return () => {
-// 			setIsMounted(false);
-// 		};
-// 	}, []);
-
-// 	const navigate = useNavigate();
-
-// 	useEffect(() => {
-// 		setErrMsg("");
-// 	}, [email, password]);
-
-// 	const handleSubmit = (e) => {
-// 		e.preventDefault();
-
-// 		fetch("http://127.0.0.1:4200/api/auth/login", {
-// 			method: "post",
-// 			headers: {
-// 				"Content-type": "application/json",
-// 			},
-// 			body: JSON.stringify({email, password}),
-// 		})
-// 			.then((res) => {
-// 				if (res.ok) {
-// 					return res.json();
-// 				} else {
-// 					throw new Error("Login failed");
-// 				}
-// 			})
-// 			.then((data) => {
-// 				if (isMounted) {
-// 					localStorage.setItem("token", data.token);
-// 					localStorage.setItem("userId", data.userId);
-// 					localStorage.setItem("isVolunteer", data.isVolunteer);
-// 					localStorage.setItem("username", data.username);
-// 					console.log(data.token, data.userId, data.isVolunteer, data.username);
-// 					navigate("/Layout");
-// 					setPassword("");
-// 					setEmail("");
-// 					setErrorMessage("");
-// 				}
-// 			})
-// 			.catch((error) => {
-// 				console.log(error);
-// 				setErrorMessage("Invalid email or password");
-// 			});
-// 	};
-
-// 	return (
-// 		<>
-// 			<section>
-// 				<Header />
-
-// 				<br />
-// 				<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>
-// 					{errMsg}
-// 				</p>
-// 				<h1>
-// 					<b>#We are here</b>
-// 				</h1>
-// 				<br />
-// 				<p className='title-bh1'>
-// 					<u>Trainee Login</u>
-// 				</p>
-// 				<form>
-// 					<label htmlFor='email'>Email:</label>
-// 					<input type='email' id='email' ref={userRef} autoComplete='off' onChange={(e) => setEmail(e.target.value)} value={email} required />
-
-// 					<label htmlFor='password'>Password:</label>
-// 					<input type='password' id='password' onChange={(e) => setPassword(e.target.value)} value={password} required />
-// 					{/* <Link to="/Layout"> */}
-// 					<button className='login__button' type='submit' onClick={handleSubmit}>
-// 						Sign In
-// 					</button>
-// 					{/* </Link> */}
-// 					{errorMessage && <p className='error'>{errorMessage}</p>}
-// 				</form>
-// 				<p>
-// 					Need an Account?
-// 					<br />
-// 					<i>Click on the button below to sign up</i>
-// 				</p>
-// 			</section>
-// 		</>
-// 	);
-// };
 
 export default VolunteerLogin;
