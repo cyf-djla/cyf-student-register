f9a245bde331cab3984b66a5fb8bf518d7bd0ab9
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-03 11:41:05 +0000

    token working correctly

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,110 +1,104 @@
-import React,{ useRef, useState, useEffect, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "./Api/axios";
+import React, {useRef, useState, useEffect, useContext} from "react";
+import {Link, useNavigate} from "react-router-dom";
 import AuthContext from "./context/AuthProvider";
 import Layout from "./StudentDashboard/Layout";
-import "./StudentDashboard/Header.css"
+import "./StudentDashboard/Header.css";
 import Header from "./StudentDashboard/Header";
-import { Route, Routes } from "react-router-dom";
-
-
-
-
-const LOGIN_URL = "http://localhost:8080/login";
+import {Route, Routes} from "react-router-dom";
 
 const Login = () => {
- 
-  const userRef = useRef();
-  const errRef = useRef();
+	const userRef = useRef();
+	const errRef = useRef();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errMsg, setErrMsg] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const [isMounted, setIsMounted] = useState(false);
 
-  const navigate=useNavigate()
+	useEffect(() => {
+		setIsMounted(true);
+		return () => {
+			setIsMounted(false);
+		};
+	}, []);
 
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    setErrMsg("");
-  }, [email, password]);
+	useEffect(() => {
+		setErrMsg("");
+	}, [email, password]);
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-  const handleSubmit =  (e) => {
-    e.preventDefault();
+		fetch("http://127.0.0.1:4200/api/auth/login", {
+			method: "post",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({email, password}),
+		})
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				} else {
+					throw new Error("Login failed");
+				}
+			})
+			.then((data) => {
+				if (isMounted) {
+					localStorage.setItem("token", data.token, "userId", data.userId, "isVolunteer", data.isVolunteer, "username", data.username);
+					console.log(data.token, data.userId, data.isVolunteer, data.username);
+					navigate("/Layout");
+					setPassword("");
+					setEmail("");
+					setErrorMessage("");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				setErrorMessage("Invalid email or password");
+			});
+	};
 
-    fetch('http://127.0.0.1:4200/api/auth/login', {
-      method: "post",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({email, password})
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
-     
-    setPassword("");
-    setEmail("");
-    setSuccess(true)
-    navigate("/Layout")
-  };
-  
+	return (
+		<>
+			<section>
+				<Header />
 
-  return (
-    <>
-     
-        <section>
-          <Header />
+				<br />
+				<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>
+					{errMsg}
+				</p>
+				<h1>
+					<b>#We are here</b>
+				</h1>
+				<br />
+				<p className='title-bh1'>
+					<u>Trainee Login</u>
+				</p>
+				<form>
+					<label htmlFor='email'>Email:</label>
+					<input type='email' id='email' ref={userRef} autoComplete='off' onChange={(e) => setEmail(e.target.value)} value={email} required />
 
-          <br />
-          <p
-            ref={errRef}
-            className={errMsg ? "errmsg" : "offscreen"}
-            aria-live="assertive"
-          >
-            {errMsg}
-          </p>
-          <h1>
-            <b>#We are here</b>
-          </h1>
-          <br />
-          <p className="title-bh1">
-            <u>Trainee Login</u>
-          </p>
-          <form >
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              required
-            />
-
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              required
-            />
-            {/* <Link to="/Layout"> */}
-              <button className="login__button" type='submit' onClick={handleSubmit}>Sign In</button>
-            {/* </Link> */}
-          </form>
-          <p>
-            Need an Account?
-            <br />
-            <i>Click on the button below to sign up</i>
-          </p>
-        </section>
-      
-    </>
-  );
+					<label htmlFor='password'>Password:</label>
+					<input type='password' id='password' onChange={(e) => setPassword(e.target.value)} value={password} required />
+					{/* <Link to="/Layout"> */}
+					<button className='login__button' type='submit' onClick={handleSubmit}>
+						Sign In
+					</button>
+					{/* </Link> */}
+					{errorMessage && <p className='error'>{errorMessage}</p>}
+				</form>
+				<p>
+					Need an Account?
+					<br />
+					<i>Click on the button below to sign up</i>
+				</p>
+			</section>
+		</>
+	);
 };
 
 export default Login;
