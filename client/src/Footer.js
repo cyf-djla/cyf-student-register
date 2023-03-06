af16ce0b63449c34eecde14d50bf5781315cf0a9
12shussein <drahimhussein@gmail.com>
2023-03-06 19:59:06 +0000

    fixed the homepage laptop and mobile landing page bug and created a footer for the homepage

---
+++ client/src/Footer.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faLinkedin, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import "./index.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="rounded-social-buttons">
+        <a className="social-button facebook" href="https://github.com/CodeYourFuture" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+        <a className="social-button twitter" href="https://twitter.com/codeyourfuture?lang=en" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a className="social-button linkedin" href="https://www.linkedin.com/company/codeyourfuture/?originalSubdomain=uk" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a className="social-button instagram" href="https://www.instagram.com/codeyourfuture_/?hl=en" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a className="social-button youtube" href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
