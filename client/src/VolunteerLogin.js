28539ea8a71baf9b406ad44e63eb57a6ba057e4f
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-06 15:58:00 +0000

    volunteer signin

--- client/src/VolunteerLogin.js
+++ client/src/VolunteerLogin.js
@@ -1,115 +1,107 @@
-import React, { useRef, useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "./Api/axios";
-import AuthContext from "./context/AuthProvider";
-import VolunteerDashboard from "./VolunteerDashboard";
+import React, {useRef, useState, useEffect, useContext} from "react";
+import {useNavigate} from "react-router-dom";
 import "./StudentDashboard/Header.css";
 import Header from "./StudentDashboard/Header";
 import "./index.css";
 
-const LOGIN_URL = "http://localhost:8080/login";
-
 const VolunteerLogin = () => {
-  const { setAuth } = useContext(AuthContext);
-  const userRef = useRef();
-  const errRef = useRef();
+	const userRef = useRef();
+	const errRef = useRef();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
-  const [validVolunteers, setValidVolunteers] = useState([]);
-  const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errMsg, setErrMsg] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    userRef.current.focus();
-    // Load valid volunteers from JSON file
-    fetch("http://127.0.0.1:4200/api/auth/volunteers")
-      .then((response) => response.json())
-      .then((data) => setValidVolunteers(data))
-      .catch((error) => console.error(error));
-  }, []);
+	useEffect(() => {
+		setIsMounted(true);
+		return () => {
+			setIsMounted(false);
+		};
+	}, []);
 
-  useEffect(() => {
-    setErrMsg("");
-  }, [username, password]);
+	const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	useEffect(() => {
+		setErrMsg("");
+	}, [email, password]);
 
-    setErrMsg("");
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    // Check if volunteer credentials are valid
-    const validVolunteer = validVolunteers.find((v) => v.username === username && v.password === password);
+		fetch("https://cyf-student-register.onrender.com/api/auth/login", {
+			method: "post",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({email, password}),
+		})
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				} else {
+					throw new Error("Login failed");
+				}
+			})
+			.then((data) => {
+				if (isMounted) {
+					localStorage.setItem("token", data.token);
+					localStorage.setItem("userId", data.userId);
+					localStorage.setItem("isVolunteer", data.isVolunteer);
+					localStorage.setItem("username", data.username);
+					console.log(data.token, data.userId, data.isVolunteer, data.username);
+					navigate("/Volunteerdashboard");
+					setPassword("");
+					setEmail("");
+					setErrorMessage("");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				setErrorMessage("Invalid email or password");
+			});
+	};
 
-    if (validVolunteer) {
-      // if volunteer credentials are valid
-      setAuth({
-        username: validVolunteer.username,
-        password: validVolunteer.password,
-        roles: ["volunteer"],
-        accessToken: "fake_access_token",
-      });
-      setUsername("");
-      setPassword("");
-      setSuccess(true);
-      navigate("/VolunteerDashboard");
-    } else {
-      // Volunteer credentials are invalid
-      setErrMsg("Invalid credentials. Please try again.");
-    }
-  };
+	return (
+		<>
+			<section>
+				<Header />
 
-  return (
-    <>
-      <section>
-        <Header />
+				<br />
+				<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>
+					{errMsg}
+				</p>
+				<h1>
+					<b>#We are here</b>
+				</h1>
+				<br />
+				<div className='title-bh1'>
+					<p className='title-bh1'>
+						<u>Volunteer Login</u>
+					</p>
+				</div>
+				<form>
+					<label htmlFor='email'>Email:</label>
+					<input type='email' id='email' ref={userRef} autoComplete='off' onChange={(e) => setEmail(e.target.value)} value={email} required />
 
-        <br />
-        <p
-          ref={errRef}
-          className={errMsg ? "errmsg" : "offscreen"}
-          aria-live="assertive"
-        >
-          {errMsg}
-        </p>
-        <h1>
-          <b>#We are here</b>
-        </h1>
-        <br />
-        <p className="title-bh1">
-          <u>Volunteer Login</u>
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            ref={userRef}
-            autoComplete="off"
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-            required
-          />
+					<label htmlFor='password'>Password:</label>
+					<input type='password' id='password' onChange={(e) => setPassword(e.target.value)} value={password} required />
 
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-          <button className="login__button">Sign In</button>
-        </form>
-        <p className = "bottom-description">
-          Need an Account?
-          <br />
-          <i>Click on the Menu at the top right to return to the home page</i>
-        </p>
-      </section>
-    </>
-  );
+					<button className='login__button' type='submit' onClick={handleSubmit}>
+						Sign In
+					</button>
+
+					{errorMessage && <p className='error'>{errorMessage}</p>}
+				</form>
+				<p className='bottom-description'>
+					Need an Account?
+					<br />
+					<i>Click on the Menu at the top right to return to the home page </i>
+				</p>
+			</section>
+		</>
+	);
 };
 
 export default VolunteerLogin;
