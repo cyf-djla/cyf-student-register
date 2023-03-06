64b446966b1cd45fda1b1f9d1b34a0011ded3367
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-06 16:31:46 +0000

    changed classmodel

--- server/models/class.js
+++ server/models/class.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const classSchema = mongoose.Schema({
-	classId: {type: Number, required: true},
 	name: {type: String, required: true},
 	cohort: {type: String},
 	time: {type: String, required: true},
 	date: {type: Date, required: true},
 	trainees: [{
-		// _id: {type: String, required: true},
+		 _id: {type: String, required: true},
 		username: {type: String, required: true},
 		logintime: {type: Date},
 		logouttime: {type: Date},
 		flags: [{type: String}]
 	  }],
 	volunteers:[{
+		_id: {type: String, required: true},
 		name: {type: String, required: true},
 		logintime: {type: Date},
 		logouttime: {type: Date}
