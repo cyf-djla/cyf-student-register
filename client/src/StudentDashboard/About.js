5896deb35e2082f32f693771ca3803e1da136234
Lunamzelo <lunamzelo@gmail.com>
2023-03-08 11:31:10 +0000

    changed text font

--- client/src/StudentDashboard/About.js
+++ client/src/StudentDashboard/About.js
@@ -13,21 +13,23 @@ const About = () => {
         <h1 className="mobile__title2">#We are here </h1>
       </div>
       <div className="about__us">
-        <h1>About Us</h1>
-        <p>
-          CODE YOUR FUTURE <br /> need to keep record of Trainee attendance,
-          punctuality and behavior during classes. This allows volunteers to
-          provide the best possible service for trainees, with expense and
-          teaching support, and mentorship.
-          <br />
-          <br />
-          <b> OUR MISSION:</b>
-          <br /> Our vision is an application that'll allow students to
-          digitally log into classes via mobile or laptop device easily and
-          efficiently. Their participation then stored to a database, accessed
-          by volunteers that'll support them in providing the best experience
-          for trainees possible.
-        </p>
+        <div className="about__container">
+          <h1>ABOUT US</h1>
+          <p>
+            CODE YOUR FUTURE <br /> need to keep record of Trainee attendance,
+            punctuality and behavior during classes. This allows volunteers to
+            provide the best possible service for trainees, with expense and
+            teaching support, and mentorship.
+            <br />
+            <br />
+            <b> OUR MISSION:</b>
+            <br /> Our vision is an application that'll allow students to
+            digitally log into classes via mobile or laptop device easily and
+            efficiently. Their participation then stored to a database, accessed
+            by volunteers that'll support them in providing the best experience
+            for trainees possible.
+          </p>
+        </div>
       </div>
     </div>
   );
