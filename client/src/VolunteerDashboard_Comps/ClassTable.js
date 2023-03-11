ed0cf70d1f34bcff51e89dfe0ff1698116281a39
Dewayne Sewell <dewaynesewell1@gmail.com>
2023-03-11 17:38:31 +0000

    prettier

--- client/src/VolunteerDashboard_Comps/ClassTable.js
+++ client/src/VolunteerDashboard_Comps/ClassTable.js
@@ -40,181 +40,179 @@ function ClassTable({ classId }) {
   };
 
   const handleFlagSubmit = async () => {
-  if (selectedRowIndex !== null && selectedFlag !== null) {
-    const selectedTrainee = filteredLogs[0].trainees[selectedRowIndex];
-    const updatedTrainee = {
-      ...selectedTrainee,
-      flags: [...selectedTrainee.flags, selectedFlag],
-    };
-    const updatedTrainees = [...filteredLogs[0].trainees];
-    updatedTrainees[selectedRowIndex] = updatedTrainee;
-    const updatedLog = {
-      ...filteredLogs[0],
-      trainees: updatedTrainees,
-    };
-    const updatedLogs = [...logs];
-    updatedLogs[updatedLogs.indexOf(filteredLogs[0])] = updatedLog;
-    setLogs(updatedLogs);
-    setSelectedRowIndex(null);
-    setSelectedFlag(null);
+    if (selectedRowIndex !== null && selectedFlag !== null) {
+      const selectedTrainee = filteredLogs[0].trainees[selectedRowIndex];
+      const updatedTrainee = {
+        ...selectedTrainee,
+        flags: [...selectedTrainee.flags, selectedFlag],
+      };
+      const updatedTrainees = [...filteredLogs[0].trainees];
+      updatedTrainees[selectedRowIndex] = updatedTrainee;
+      const updatedLog = {
+        ...filteredLogs[0],
+        trainees: updatedTrainees,
+      };
+      const updatedLogs = [...logs];
+      updatedLogs[updatedLogs.indexOf(filteredLogs[0])] = updatedLog;
+      setLogs(updatedLogs);
+      setSelectedRowIndex(null);
+      setSelectedFlag(null);
 
-    try {
-      const response = await fetch(`https://cyf-student-register.onrender.com/api/classes/postflag/${selectedTrainee._id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ flag: selectedFlag })
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+      try {
+        const response = await fetch(
+          `https://cyf-student-register.onrender.com/api/classes/postflag/${selectedTrainee._id}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ flag: selectedFlag }),
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
     }
-  }
-};
+  };
 
+  //   const handleFlagChange = (event) => {
+  //     setSelectedFlag(event);
+  //   };
 
+  //   const handleFlagSubmit = async () => {
+  //   if (selectedRowIndex !== null && selectedFlag !== null) {
+  //     const selectedTrainee = filteredLogs[0].trainees[selectedRowIndex];
+  //     const updatedTrainee = {
+  //       ...selectedTrainee,
+  //       flags: [...selectedTrainee.flags, selectedFlag],
+  //     };
+  //     const updatedTrainees = [...filteredLogs[0].trainees];
+  //     updatedTrainees[selectedRowIndex] = updatedTrainee;
+  //     const updatedLog = {
+  //       ...filteredLogs[0],
+  //       trainees: updatedTrainees,
+  //     };
+  //     const updatedLogs = [...logs];
+  //     updatedLogs[updatedLogs.indexOf(filteredLogs[0])] = updatedLog;
+  //     setLogs(updatedLogs);
+  //     setSelectedRowIndex(null);
+  //     setSelectedFlag(null);
 
-//   const handleFlagChange = (event) => {
-//     setSelectedFlag(event);
-//   };
+  //     try {
+  //       const response = await fetch(`https://cyf-student-register.onrender.com/api/classes/postflag/${selectedTrainee._id}`, {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json"
+  //         },
+  //         body: JSON.stringify({ flag: selectedFlag })
+  //       });
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  // };
 
-//   const handleFlagSubmit = async () => {
-//   if (selectedRowIndex !== null && selectedFlag !== null) {
-//     const selectedTrainee = filteredLogs[0].trainees[selectedRowIndex];
-//     const updatedTrainee = {
-//       ...selectedTrainee,
-//       flags: [...selectedTrainee.flags, selectedFlag],
-//     };
-//     const updatedTrainees = [...filteredLogs[0].trainees];
-//     updatedTrainees[selectedRowIndex] = updatedTrainee;
-//     const updatedLog = {
-//       ...filteredLogs[0],
-//       trainees: updatedTrainees,
-//     };
-//     const updatedLogs = [...logs];
-//     updatedLogs[updatedLogs.indexOf(filteredLogs[0])] = updatedLog;
-//     setLogs(updatedLogs);
-//     setSelectedRowIndex(null);
-//     setSelectedFlag(null);
+  return (
+    <div className="class-logs">
+      {filteredLogs.length === 0 ? (
+        <p>No logs found for this class</p>
+      ) : (
+        <>
+          <table className="class-logs-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Flag</th>
+                <th>Login Time</th>
+                <th>Logout Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredLogs[0].trainees
+                .filter((trainee) => shouldShowRow(trainee))
+                .map((trainee, index) => (
+                  <tr
+                    key={trainee._id}
+                    onClick={() => handleRowClick(index)}
+                    className={selectedRowIndex === index ? "selected" : ""}
+                  >
+                    <td>{trainee._id}</td>
+                    <td>{trainee.username}</td>
+                    <td>{trainee.flags.join(", ")}</td>
+                    <td>
+                      {trainee.logintime
+                        ? moment(trainee.logintime).format("h:mm a")
+                        : "-"}
+                    </td>
+                    <td>
+                      {trainee.logouttime
+                        ? moment(trainee.logouttime).format("h:mm a")
+                        : "Still logged in"}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
 
-//     try {
-//       const response = await fetch(`https://cyf-student-register.onrender.com/api/classes/postflag/${selectedTrainee._id}`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({ flag: selectedFlag })
-//       });
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-// };
-
- return (
-  <div className="class-logs">
-    {filteredLogs.length === 0 ? (
-      <p>No logs found for this class</p>
-    ) : (
-      <>
-        <table className="class-logs-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Username</th>
-              <th>Flag</th>
-              <th>Login Time</th>
-              <th>Logout Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredLogs[0].trainees
-              .filter((trainee) => shouldShowRow(trainee))
-              .map((trainee, index) => (
-                <tr
-                  key={trainee._id}
-                  onClick={() => handleRowClick(index)}
-                  className={selectedRowIndex === index ? "selected" : ""}
-                >
-                  <td>{trainee._id}</td>
-                  <td>{trainee.username}</td>
-                  <td>{trainee.flags.join(", ")}</td>
-                  <td>
-                    {trainee.logintime ? (
-                      moment(trainee.logintime).format("h:mm a")
-                    ) : (
-                      "-"
-                    )}
-                  </td>
-                  <td>
-                    {trainee.logouttime ? (
-                      moment(trainee.logouttime).format("h:mm a")
-                    ) : (
-                      "Still logged in"
-                    )}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-
-        {selectedRowIndex !== null && (
-          <div className="flag_box">
-            <h3>Flag Trainee:</h3>
-            <label>
-              <input
-                type="radio"
-                className="radio-button"
-                name="flags"
-                value="Late"
-                onChange={(e) => handleFlagChange(e.target.value)}
-              />
-              Late
-            </label>
-            <label>
-              <input
-                type="radio"
-                className="radio-button"
-                name="flags"
-                value="Left early"
-                onChange={(e) => handleFlagChange(e.target.value)}
-              />
-              Left early
-            </label>
-            <label>
-              <input
-                type="radio"
-                className="radio-button"
-                name="flags"
-                value="Not participating"
-                onChange={(e) => handleFlagChange(e.target.value)}
-              />
-              Not participating
-            </label>
-            <label>
-              <input
-                type="radio"
-                className="radio-button"
-                name="flags"
-                value="Absent/Camera off"
-                onChange={(e) => handleFlagChange(e.target.value)}
-              />
-              Absent/Camera off
-            </label>
-             <button className= "table_button" onClick={handleFlagSubmit}>Submit</button>
-          </div>
-        )}
-      </>
-    )}
-  </div>
-);
-
-};
+          {selectedRowIndex !== null && (
+            <div className="flag_box">
+              <h3>Flag Trainee:</h3>
+              <label>
+                <input
+                  type="radio"
+                  className="radio-button"
+                  name="flags"
+                  value="Late"
+                  onChange={(e) => handleFlagChange(e.target.value)}
+                />
+                Late
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  className="radio-button"
+                  name="flags"
+                  value="Left early"
+                  onChange={(e) => handleFlagChange(e.target.value)}
+                />
+                Left early
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  className="radio-button"
+                  name="flags"
+                  value="Not participating"
+                  onChange={(e) => handleFlagChange(e.target.value)}
+                />
+                Not participating
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  className="radio-button"
+                  name="flags"
+                  value="Absent/Camera off"
+                  onChange={(e) => handleFlagChange(e.target.value)}
+                />
+                Absent/Camera off
+              </label>
+              <button className="table_button" onClick={handleFlagSubmit}>
+                Submit
+              </button>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
 
 export default ClassTable;
