dd87625c704ff0b0cefe327a366ded1079e879a9
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-11 15:50:54 +0000

    not working

--- client/src/StudentDashboard/About.js
+++ client/src/StudentDashboard/About.js
@@ -14,31 +14,38 @@ const About = () => {
         /> 
         <h1 className="mobile__title2">#We are here </h1>
       </div> */}
-      <div className="about__hero-image">
-        <h1 className="home__title">#We are here</h1>
-      </div>
-      <div className="about__us">
+      {/* <div className="about__hero-image">
         
+      </div> */}
+      <div className="about__us">
+      {/* <h1 className="home__title">#We are here</h1> */}
         
         <div className="about__ustext">
+         
+          
           <h1 className='header_text'>About us</h1>
-          <p>
             <h5>-CODE YOUR FUTURE- </h5>
+            <p>
             Our Volunteers need to keep record of Trainee attendance,
             punctuality and behavior during classes. This allows volunteers to
             provide the best possible service for our trainees with teaching
             support, mentorship and covering expenses.
+            </p>
+           
            
             <h5> OUR MISSION:</h5>
-           Our vision is an application that'll allow students to
+            <p>
+            Our vision is an application that'll allow students to
             digitally log into classes via mobile or laptop device easily and
             efficiently. A record of their participation then stored to a
             database, accessed by volunteers that'll support them in providing
             the best experience for trainees possible.
-          </p>
+            </p>
+          
+          
         </div>
       </div>
-    </div>
+     </div>
   );
 }
 
